103b62380f2580f3a33697a1dfeeb3270c3c7255
Patrick Castro <patrickcastro117@yahoo.com>
2022-02-10 16:46:36 -0800

    Included Material UI 5

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
+import Button from '@mui/material/Button';
 import './App.css';
+import HomePage from './Components/HomePage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HomePage />
+      <Button variant='contained'>Click Me!</Button>
     </div>
   );
 }
