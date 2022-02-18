6200d0ea24327647d7acaaf3c33c1500ae96acbc
Patrick Castro <patrickcastro117@yahoo.com>
2022-02-17 16:34:05 -0800

    Added Tab Navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import Button from '@mui/material/Button';
 import './SASS/App.scss';
+import './SASS/TabBar.scss';
 import HomePage from './Components/HomePage';
+import FullWidthTabs from './Components/Tabs';
 
 function App() {
   return (
     <div className="App">
+      <FullWidthTabs />
       <HomePage />
       <Button variant='contained'>Click Me!</Button>
     </div>
